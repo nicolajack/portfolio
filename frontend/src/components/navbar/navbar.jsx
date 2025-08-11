5a993cc605fbba18bc2bae786cb81da9cf539f4d
nicolajack <nicolajack@gmail.com>
2025-08-11 09:54:39 -0400

    added fun animation

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -3,7 +3,7 @@ import './navbar.css'
 function Navbar() {
     return (
         <div id="navbar">
-            <h1><em>n</em>ico jackso<em>n</em>*</h1>
+            <h1 id="name" onClick={() => window.open('https://www.instagram.com/nicojqckson/', '_blank')}><em>n</em>ico jackso<em>n</em>*</h1>
             <a href="#about">about</a>
             <a href="#projects">projects</a>
             <a href="#exp">experiences</a>
