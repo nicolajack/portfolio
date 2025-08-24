3e9411011661b5ad397367ae486057a2b24b5647
nicolajack <nicolajack@gmail.com>
2025-08-24 18:01:20 -0400

    added links to project websites

--- frontend/src/components/projects/projects.jsx
+++ frontend/src/components/projects/projects.jsx
@@ -1,11 +1,12 @@
 import './projects.css'
 
-function Projects({ name, description, github }) {
+function Projects({ name, description, github, deployLink }) {
     return (
             <div className="project">
                 <h4>{name}</h4>
                 <p>{description}</p>
                 <a target="_blank" href={github}><button className="github"><img className="github-inline" width="20" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" /> GitHub</button></a>
+                <a target="_blank" href={deployLink}><button className="deploy">Live Demo</button></a>
             </div>
     )
 }
