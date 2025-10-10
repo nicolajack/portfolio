cf858e059d5b19b93df1a87e81bedf73887783bb
nicolajack <nicolajack@gmail.com>
2025-10-10 18:21:53 -0400

    updated my skills to look cleaner

--- frontend/src/components/about/about.jsx
+++ frontend/src/components/about/about.jsx
@@ -9,6 +9,14 @@ import C from '../../assets/C.png'
 import PANDAS from '../../assets/pandas.png'
 import NUMPY from '../../assets/NumPy.png'
 import ReactLogo from '../../assets/atom.png'
+import Python from '../../assets/python.png'
+import TypeScript from '../../assets/typescript.png'
+import Java from '../../assets/java.png'
+import JavaScript from '../../assets/java-script.png'
+import Html from '../../assets/html.png'
+import C2 from '../../assets/letter-c.png'
+import Figma from '../../assets/figma.png'
+import SQL2 from '../../assets/sql-server.png'
 import { useState } from 'react'
 import LogoLoop from './logoloop.jsx';
 
@@ -27,7 +35,51 @@ function About(){
     }
 
     const techLogos = [
-        { node: ReactLogo, title: "React", href: "https://react.dev" },
+        { 
+            node: <img src={ReactLogo} alt="React" style={{ height: '48px', width: 'auto' }} />, 
+            title: "React", 
+            href: "https://react.dev" 
+        },
+        {
+            node: <img src={Python} alt="Python" style={{ height: '48px', width: 'auto'}} />,
+            title: "Python",
+            href: "https://www.python.org/",
+        },
+        {
+            node: <img src={TypeScript} alt="TypeScript" style={{ height: '48px', width: 'auto'}} />,
+            title: "TypeScript",
+            href: "https://www.typescriptlang.org/",
+        },
+        {
+            node: <img src={Java} alt="Java" style={{ height: '48px', width: 'auto'}} />,
+            title: "Java",
+            href: "https://www.java.com/en/",
+        },
+        {
+            node: <img src={JavaScript} alt="JavaScript" style={{ height: '48px', width: 'auto'}} />,
+            title: "JavaScript",
+            href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+        },
+        {
+            node: <img src={Html} alt="HTML" style={{ height: '48px', width: 'auto'}} />,
+            title: "HTML",
+            href: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+        },
+        {
+            node: <img src={C2} alt="C" style={{ height: '48px', width: 'auto'}} />,
+            title: "C",
+            href: "https://www.w3schools.com/c/c_intro.php",
+        },
+        {
+            node: <img src={Figma} alt="Figma" style={{ height: '48px', width: 'auto'}} />,
+            title: "Figma",
+            href: "https://www.figma.com/",
+        },
+        {
+            node: <img src={SQL2} alt="SQL" style={{ height: '48px', width: 'auto'}} />,
+            title: "SQL",
+            href: "https://aws.amazon.com/what-is/sql/",
+        }
     ];
 
     return (
@@ -44,36 +96,18 @@ function About(){
                 </p>
                 <br/>
                 <h3>my skills</h3>
-                {/*
-                <div style={{ height: '200px', position: 'relative', overflow: 'hidden'}}>
+                <div style={{ position: 'relative', overflow: 'visible', padding: '10px 0'}}>
                     <LogoLoop
                         logos={techLogos}
-                        speed={120}
+                        speed={100}
                         direction="left"
                         logoHeight={48}
                         gap={40}
                         pauseOnHover
                         scaleOnHover
                         fadeOut
-                        fadeOutColor="#ffffff"
                         ariaLabel="Technology partners"
                     />
-                </div>
-                */}
-                <div className="icons">
-                    <img id="icon" width="60" src={CSS} alt="css" />
-                    <img id="icon" width="60" src={HTML} alt="html" />
-                    <img id="icon" width="60" src={JS} alt="js" />
-                    <img id="icon" width="60" src={TS} alt="ts" />
-                    <img id="icon" width="60" src={PY} alt="python" />
-                    <img id="icon" width="60" src={C} alt="c" />
-                </div>
-                <br />
-                <h3>currently learning</h3>
-                <div className="icons">
-                    <img id="icon" width="60" src={PANDAS} alt="pandas" />
-                    <img id="icon" width="60" src={SQL} alt="sql" />
-                    <img id="icon" width="60" src={NUMPY} alt="numpy" />
                 </div>
             </div>
         </div>
